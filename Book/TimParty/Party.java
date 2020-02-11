abe9bd57abe18571eb963bf1f349b207454a1cd2
BotmodeOn <BotmodeOn@users.noreply.github.com>
2020-02-11 05:55:08 +0300

    Add files via upload

---
+++ Book/TimParty/Party.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.awt.*;
+import java.awt.event.*;
+
+import java.awt.*;
+
+public class Party {
+    public void buildinvait() {
+        Frame f = new Frame();
+        Label l = new Label("Tom Party");
+        Button b = new Button("Your bet");
+        Button c = new Button("Reset");
+        Panel p = new Panel();
+        p.add(l);
+    }
+}
